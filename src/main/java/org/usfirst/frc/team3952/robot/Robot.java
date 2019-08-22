ed3452ddaa3549170e6f4d46bc7fe8d4fdb10a01
DoggySazHi <william.le.huy@gmail.com>
2019-08-21 21:02:16 -0700

    NT Connection? - DoggySazHi

--- src/main/java/org/usfirst/frc/team3952/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team3952/robot/Robot.java
@@ -14,6 +14,10 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import org.usfirst.frc.team3952.robot.subsystems.*;
+
+import java.time.Duration;
+import java.time.Instant;
+
 import org.usfirst.frc.team3952.robot.commands.*;
 
 public class Robot extends TimedRobot {
@@ -26,15 +30,18 @@ public class Robot extends TimedRobot {
 
     public static NetworkTableInstance ntInst;
     public static NetworkTable nTable;
-    public static NetworkTableEntry autoAlignX;
-    public static NetworkTableEntry autoAlignY;
+    public static NetworkTableEntry vector;
+
+    private boolean mainControllerInit;
+    private boolean ladderControllerInit;
+    private Instant checkControllerTime;
+    private final long CHECK_DELAY = 5000;
 
     @Override
     public void robotInit() {
         ntInst = NetworkTableInstance.getDefault();
-        nTable = ntInst.getTable("datatable");
-        autoAlignX = nTable.getEntry("movex");
-        autoAlignY = nTable.getEntry("movey");
+        nTable = ntInst.getTable("LimeLightLite");
+        vector = nTable.getEntry("AutoAlign Coordinates");
         
         RobotMap.init();
         driveTrain = new DriveTrain();
@@ -44,6 +51,7 @@ public class Robot extends TimedRobot {
         try
         {
             mainController = new MainController(new Joystick(0));
+            mainControllerInit = true;
         }
         catch(Exception ex)
         {
@@ -54,6 +62,7 @@ public class Robot extends TimedRobot {
         try
         {
             ladderController = new LadderController(new Joystick(1));
+            ladderControllerInit = true;
         }
         catch(Exception ex)
         {
@@ -70,7 +79,39 @@ public class Robot extends TimedRobot {
     }
 
     @Override
-    public void robotPeriodic() {}
+    public void robotPeriodic() {
+        if(checkControllerTime == null)
+            checkControllerTime = Instant.now();
+        else if(!(mainControllerInit && ladderControllerInit))
+            if(Duration.between(checkControllerTime, Instant.now()).toMillis() > CHECK_DELAY)
+            {
+                if(!mainControllerInit)
+                {
+                    try
+                    {
+                        mainController = new MainController(new Joystick(0));
+                        mainControllerInit = true;
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println("Failed to connect main controller. Retrying later.");
+                    }
+                }
+                if(!ladderControllerInit)
+                {
+                    try
+                    {
+                        ladderController = new LadderController(new Joystick(1));
+                        ladderControllerInit = true;
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println("Failed to connect ladder controller. Retrying later.");
+                    }
+                }
+                checkControllerTime = Instant.now();
+            }
+    }
 
     @Override
     public void disabledInit() {}
@@ -98,7 +139,7 @@ public class Robot extends TimedRobot {
     @Override
     public void testPeriodic() { Scheduler.getInstance().run(); }
 
-    public static int[] distanceToCenter() {
-        return new int[] {autoAlignX.getNumber(0).intValue(), autoAlignY.getNumber(0).intValue()};
+    public static double[] distanceToCenter() {
+        return vector.getDoubleArray(new double[] {0.0, 0.0});
     }
 }
