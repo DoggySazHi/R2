8911ab9d31da6a8fb32ae4cf252031e8dddf3524
William Le <800020592@fjuhsd.org>
2020-01-22 16:23:47 -0800

    Commenting, part 1/???

--- src/main/java/org/usfirst/frc/team3952/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team3952/robot/RobotMap.java
@@ -9,64 +9,33 @@ import edu.wpi.first.wpilibj.util.Color;
 //It's sparks, not talons lucas, ya dumbie
 
 public class RobotMap {
-
-    //PWM
-    public static final int RIGHT_DRIVE_PORT = 0;
-    public static final int LEFT_DRIVE_PORT = 1;
-    public static final int PROJECTILE_EJECTION_PORT = 2;
-    public static final int PROJECTILE_AIM_SERVO_PORT = 3;
-    public static final int PROJECTILE_STORAGE_PORT = 4;
-    public static final int TILT_LINEAR_ACTUATOR_PORT = 8;
-    public static final int CONTROL_PANEL_SPINNER_PORT = 6;
-
-    // u/DIO_ships_renmari
-    public static final int LIMIT_SWITCH_ENABLE_SHOOTER_PORT = 0;
-    public static final int LIMIT_SWITCH_DISABLE_SHOOTER_PORT = 1;
-
-    //CAN
-    public static final int INTAKE_PORT = 0;
-    public static final int INTAKE_TWO_PORT = 1;
-
-    //ANALOG IN
-    public static final int TILT_LINEAR_ACTUATOR_ENCODER = 1;
-
-    //PNEUMATIC BOARD (PCM)
-
-    // *chirp* *chirp*
-
     // Robot Values
 
     // Milliseconds before re-checking for controllers.
     public static final long CONTROLLER_CHECK_DELAY = 5000;
 
     // Control Panel Values
+
+    // Colors represented by the color sensor.
     public static final Color CP_RED = ColorMatch.makeColor(0.475, 0.370, 0.150);
     public static final Color CP_GREEN = ColorMatch.makeColor(0.190, 0.545, 0.260);
     public static final Color CP_BLUE = ColorMatch.makeColor(0.150, 0.455, 0.385);
     public static final Color CP_YELLOW = ColorMatch.makeColor(0.300, 0.550, 0.160);
+
+    // The color pattern of the Control Panel wheel.
     public static final Color[] WHEEL = new Color[]{CP_RED, CP_GREEN, CP_BLUE, CP_YELLOW};
+
+    // The amount of times to spin before actually finding the right color.
     public static final int MIN_COUNT = 24;
+
+    // Speeds to set the motors when running, if not using a linear ramping function.
     public static final double CW_SPEED_FAST = 0.5;
     public static final double CW_SPEED_SLOW = 0.2;
+
+    // Tiles to start slowing down, if not using a linear ramping function.
     public static final int SLOWDOWN_TILES = 5;
 
-    // Shooter Values
-    public static final int MAX_BALL_STORAGE = 5;
-
-    // Drive Train
-    public static Spark leftDrive;
-    public static Spark rightDrive;
-
-    // Shooter Superstructure
-    public static Talon projectileEjector;
-    public static Servo projectileAimer;
-    public static Talon projectileStorage;
-    public static Talon projectileTilt;
-    public static Talon controlPanelSpinner;
-
-    public static DigitalInput enableShooter;
-    public static DigitalInput disableShooter;
-
+    // Holds the possible positions for the shooter's angle with given names.
     public enum Position {
         Intake (0.0),
         Shooting (40.0),
@@ -85,6 +54,25 @@ public class RobotMap {
         }
     }
 
+    // Shooter Values
+
+    // The amount of balls that can be stored in the shooter.
+    public static final int MAX_BALL_STORAGE = 5;
+
+    // Drive Train (wheels are bundled together on the same PWM)
+    public static Spark leftDrive;
+    public static Spark rightDrive;
+
+    // Shooter Superstructure
+    public static Talon projectileEjector;
+    public static Talon projectileStorage;
+    public static Talon projectileTilt;
+    public static Talon controlPanelSpinner;
+
+    public static DigitalInput enableShooter;
+    public static DigitalInput disableShooter;
+    public static DigitalInput spinnerLocked;
+
     // Misc
     public static AnalogEncoder linearActuatorEncoder;
     public static VictorSPX intake;
@@ -94,21 +82,31 @@ public class RobotMap {
     public static ColorSensorV3 colorSensor;
 
     public static void init() {
-        leftDrive = new Spark(RIGHT_DRIVE_PORT);
-        rightDrive = new Spark(LEFT_DRIVE_PORT);
-        projectileTilt = new Talon(TILT_LINEAR_ACTUATOR_PORT);
-        projectileAimer = new Servo(PROJECTILE_AIM_SERVO_PORT);
-        projectileStorage = new Talon(PROJECTILE_STORAGE_PORT);
-        projectileEjector = new Talon(PROJECTILE_EJECTION_PORT);
-        controlPanelSpinner = new Talon(CONTROL_PANEL_SPINNER_PORT);
+        // PWM (Motors and Servos)
 
-        enableShooter = new DigitalInput(LIMIT_SWITCH_ENABLE_SHOOTER_PORT);
-        disableShooter = new DigitalInput(LIMIT_SWITCH_DISABLE_SHOOTER_PORT);
+        leftDrive = new Spark(1);
+        rightDrive = new Spark(0);
+        projectileEjector = new Talon(2);
+        projectileAimer = new Servo(3);
+        projectileStorage = new Talon(4);
+        projectileTilt = new Talon(5);
+        controlPanelSpinner = new Talon(6);
 
-        linearActuatorEncoder = new AnalogEncoder(new AnalogInput(TILT_LINEAR_ACTUATOR_ENCODER));
+        // DIO (Limit switches)
 
-        intake = new VictorSPX(INTAKE_PORT);
-        intake2 = new VictorSPX(INTAKE_TWO_PORT);
+        enableShooter = new DigitalInput(0);
+        disableShooter = new DigitalInput(1);
+        spinnerLocked = new DigitalInput(2);
+
+
+        linearActuatorEncoder = new AnalogEncoder(new AnalogInput(0));
+
+        // CAN
+
+        intake = new VictorSPX(0);
+        intake2 = new VictorSPX(1);
+
+        // Other sensors
 
         colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
     }
