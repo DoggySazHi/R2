035c53b91bd8e0cb402aaa1f037417ff06eda53c
DoggySazHi <doggysazhi@gmail.com>
2020-01-23 15:43:37 -0800

    comments

--- src/main/java/org/usfirst/frc/team3952/robot/subsystems/IntakeShooter.java
+++ src/main/java/org/usfirst/frc/team3952/robot/subsystems/IntakeShooter.java
@@ -8,8 +8,9 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.usfirst.frc.team3952.robot.RobotMap;
+import org.usfirst.frc.team3952.robot.RobotMap.Position;
 
-import static org.usfirst.frc.team3952.robot.RobotMap.linearActuatorEncoder;
+import static org.usfirst.frc.team3952.robot.RobotMap.MAX_BALL_STORAGE;
 
 public class IntakeShooter extends SubsystemBase {
     public static final double INTAKE_SPEED = 1.0;
@@ -25,7 +26,9 @@ public class IntakeShooter extends SubsystemBase {
 
     private DigitalInput spinnerLocked = RobotMap.spinnerLocked;
 
-    private int ballsStored;
+    private boolean[] ballsStored = new boolean[MAX_BALL_STORAGE];
+
+    private int ballPosition = 0;
 
     public void intake() {
         intakeLeft.set(ControlMode.PercentOutput, -1.0 * INTAKE_SPEED);
@@ -43,40 +46,65 @@ public class IntakeShooter extends SubsystemBase {
         angleMotor.stopMotor();
     }
 
+    // Control the storage motor. Should be combined with getPosition().
     public void setAngleMotor(double speed) {
         angleMotor.set(speed);
     }
 
+    // Control the tilt (ball shooting) servos.
     public void setTiltServos(double speed) {
         tiltServos.set(speed);
     }
 
-    public double getPositionRaw() {
-        return linearActuatorEncoder.get();
+    // Control the storage motor to find a slot. Should be combined with isLocked().
+    public void setRotateMotor(double speed) {
+        spinnerMotor.set(speed);
     }
 
+    // Check if the ball holder is currently on a valid position (not half open)
+    public boolean isLocked() {
+        return spinnerLocked.get();
+    }
+
+    // Check if there's a ball stored in the current position.
+    public boolean ballInPosition() {
+        return ballsStored[ballPosition];
+    }
+
+    // Count the amount of balls that are in the IntakeShooter.
+    public int getBalls() {
+        int balls = 0;
+        for(boolean b : ballsStored) if(b) balls++;
+        return balls;
+    }
+
+    // Advance the counter. NOTE: You should be running the motor before this is called!
+    public void advance() {
+        ballPosition = (ballPosition + 1) % ballsStored.length;
+    }
+
+    // Rewind the counter. NOTE: You should be running the motor before this is called!
+    public void rewind() {
+        ballPosition = (ballPosition + ballsStored.length - 1) % ballsStored.length;
+    }
+
+    // Get the raw value of the actuator's encoder.
+    public double getPositionRaw() {
+        return liftMotorEncoder.get();
+    }
+
+    // Get the current "angle" of the shooter position. Might be removed in favor of Position (see RobotMap)
     public double getAngle() {
         //TODO haoyan doesn't know how to write code
         return 0;
     }
 
-    public boolean isLocked() {
-        return spinnerLocked.get();
-    }
-
-    public void setBallsStored(int ballsStored) {
-        this.ballsStored = ballsStored;
-    }
-
-    public int getBallsStored() {
-        return ballsStored;
-    }
-
-    public RobotMap.Position getPosition()
+    // Get the current position of the shooter's angle.
+    public Position getPosition()
     {
-        RobotMap.Position toReturn = RobotMap.Position.values()[0];
-        double encoder = linearActuatorEncoder.get();
-        for(RobotMap.Position p : RobotMap.Position.values()) {
+        Position toReturn = Position.values()[0];
+        double encoder = liftMotorEncoder.get();
+        for(Position p : Position.values()) {
             if (Math.abs(p.getDistance() - encoder) < Math.abs(toReturn.getDistance() - encoder))
                 toReturn = p;
         }
