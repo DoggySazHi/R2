1582d73399a87706dfdb530f00e343c833d34f47
DoggySazHi <doggysazhi@gmail.com>
2020-02-11 22:25:17 -0800

    Some changes to the AnalogUltrasonic, for custom NT support. <:reimuthink:629869106006327303>

--- src/main/java/org/usfirst/frc/team3952/robot/devices/AnalogUltrasonic.java
+++ src/main/java/org/usfirst/frc/team3952/robot/devices/AnalogUltrasonic.java
@@ -1,14 +1,22 @@
 package org.usfirst.frc.team3952.robot.devices;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 public class AnalogUltrasonic {
     private AnalogInput ai;
+    private NetworkTableEntry ntStorage;
+
     public AnalogUltrasonic(int analogPin)
     {
         ai = new AnalogInput(analogPin);
+    }
+
+    public AnalogUltrasonic withNTData(String table, String entry)
+    {
+        ntStorage = NetworkTableInstance.getDefault().getTable(table).getEntry(entry);
+        return this;
     }
 
     private static final double toInch = 0.125;
@@ -17,7 +25,8 @@ public class AnalogUltrasonic {
     public double getRangeMM()
     {
         double distance = ai.getValue() * toInch * inchToCM * 100;
-        NetworkTableInstance.getDefault().getTable(":patchythink:").getEntry(":reimuthink:").setDouble(distance);
+        if(ntStorage != null)
+            ntStorage.setDouble(distance);
         return distance;
     }
 }
