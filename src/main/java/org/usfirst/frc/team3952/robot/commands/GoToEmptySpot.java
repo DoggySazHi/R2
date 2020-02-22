5604a7c925a8c44bd290a78559a2a01556d60520
Kendra Chen <800021455@fjuhsd.org>
2020-02-22 15:38:28 -0800

    added indexing

---
+++ src/main/java/org/usfirst/frc/team3952/robot/commands/GoToEmptySpot.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team3952.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import static org.usfirst.frc.team3952.robot.RobotMap.STORAGE_MOTOR_SPEED;
+import org.usfirst.frc.team3952.robot.subsystems.IntakeShooter;
+import org.usfirst.frc.team3952.robot.subsystems.RobotSubsystems;
+
+public class GoToEmptySpot extends CommandBase {
+    private RobotSubsystems subsystems;
+    private boolean wasLocked;
+    public GoToEmptySpot(RobotSubsystems subsystems) {
+        this.subsystems = subsystems;
+        IntakeShooter shooter = subsystems.getIntakeShooter();
+        addRequirements(shooter);
+    }
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        IntakeShooter shooter = subsystems.getIntakeShooter();
+            shooter.setRotateMotor(STORAGE_MOTOR_SPEED);   
+            if(shooter.isLocked() && !wasLocked)  
+            {
+                shooter.advance();
+                wasLocked = true;
+            }
+            else if(!shooter.isLocked() && wasLocked)
+            {
+                wasLocked = false;   
+            }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !subsystems.getIntakeShooter().ballInPosition();
+    }
+}
