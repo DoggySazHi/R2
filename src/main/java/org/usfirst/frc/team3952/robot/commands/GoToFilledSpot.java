d68b68354cf553ff6c4cb5954088948c2bb406df
Shrey Gandhi <600021856@fjuhsd.org>
2020-02-24 16:23:49 -0800

    It takes the robot to the next position.

--- src/main/java/org/usfirst/frc/team3952/robot/commands/GoToFilledSpot.java
+++ src/main/java/org/usfirst/frc/team3952/robot/commands/GoToFilledSpot.java
@@ -1,10 +1,10 @@
 package org.usfirst.frc.team3952.robot.commands;
 import org.usfirst.frc.team3952.robot.subsystems.RobotSubsystems;
 
-public class GoToOpenSpot extends GoToEmptySpot {
+public class GoToFilledSpot extends GoToEmptySpot {
     private RobotSubsystems subsystems;
 
-    public GoToOpenSpot(RobotSubsystems subsystems) {
+    public GoToFilledSpot(RobotSubsystems subsystems) {
         super(subsystems);
         this.subsystems = subsystems;
     }
--- src/main/java/org/usfirst/frc/team3952/robot/commands/GoToNextPosition.java
+++ src/main/java/org/usfirst/frc/team3952/robot/commands/GoToNextPosition.java
@@ -4,10 +4,11 @@ import static org.usfirst.frc.team3952.robot.RobotMap.STORAGE_MOTOR_SPEED;
 import org.usfirst.frc.team3952.robot.subsystems.IntakeShooter;
 import org.usfirst.frc.team3952.robot.subsystems.RobotSubsystems;
 
-public class GoToEmptySpot extends CommandBase {
+public class GoToNextPosition extends CommandBase {
+    private boolean nextLevel;
     private RobotSubsystems subsystems;
     private boolean wasLocked;
-    public GoToEmptySpot(RobotSubsystems subsystems) {
+    public GoToNextPosition(RobotSubsystems subsystems) {
         this.subsystems = subsystems;
         IntakeShooter shooter = subsystems.getIntakeShooter();
         addRequirements(shooter);
@@ -19,23 +20,24 @@ public class GoToEmptySpot extends CommandBase {
     @Override
     public void execute() {
         IntakeShooter shooter = subsystems.getIntakeShooter();
-            shooter.setRotateMotor(STORAGE_MOTOR_SPEED);   
-            if(shooter.isLocked() && !wasLocked)  
-            {
-                shooter.advance();
-                wasLocked = true;
-            }
-            else if(!shooter.isLocked() && wasLocked)
-            {
-                wasLocked = false;   
-            }
+        shooter.setRotateMotor(STORAGE_MOTOR_SPEED);   
+        if(shooter.isLocked() && !wasLocked)  
+        {
+            shooter.advance();
+            wasLocked = true;
+        }
+        else if(!shooter.isLocked() && wasLocked)
+        {
+            wasLocked = false;
+            nextLevel = true;   
+        }
     }
 
     @Override
     public boolean isFinished() {
-        return !subsystems.getIntakeShooter().ballInPosition();
+        return nextLevel;
     }
-
+    
     @Override
     public void end(boolean interruptable) {
         IntakeShooter shooter = subsystems.getIntakeShooter();
