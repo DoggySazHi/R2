67561c9cef48af2cb41ceaace9c1e3a2ab8e7bfd
Bryce + Haoyan <bryce@mechhead.com>
2020-03-07 11:29:37 -0800

    Changes???

--- src/main/java/org/usfirst/frc/team3952/robot/commands/ManualTurn.java
+++ src/main/java/org/usfirst/frc/team3952/robot/commands/ManualTurn.java
@@ -34,17 +34,4 @@ public class ManualTurn extends CommandBase
         double rot = mainController.getHorizontalMovement();
         controlWheel.set(rot);
         Color c = controlWheel.getColor();
-        colorValue.setDoubleArray(new double[] {c.red, c.green, c.blue});
-    }
-
-    public boolean isFinished() {
-        return false;
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        ControlWheel controlWheel = subsystems.getControlWheel();
-        controlWheel.stop();
-        controlWheel.disable();
-    }
-}
+        colorValue.setDoubleArray(new double[] {c.red, c.green, c.blue})
